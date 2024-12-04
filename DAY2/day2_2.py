9637a39afa84ab7ad2184b4cb7e5be2eb376d6c2
angur001 <70037460+angur001@users.noreply.github.com>
2024-12-04 01:18:54 +0100

    correct day2

--- DAY2/day2_2.py
+++ DAY2/day2_2.py
@@ -7,17 +7,11 @@ def isValidLevel(arr):
             return False
     return True
 
-def IsValidLevelWithError(arr):
-    isIncreasing = (arr[1] > arr[0])
-    for i in range(len(arr)-1):
-        if (0 < abs(arr[i+1] - arr[i]) <= 3 and not( (arr[i+1] > arr[i]) ^ isIncreasing )):
-            continue
-        else:
-            if (isValidLevel(arr[:i] + arr[i+1:])) or (i == len(arr)-2 and isValidLevel(arr[:i+1])):
-                return True
-            else:
-                return False 
-    return True
+def IsValidLevelWithErrorForce(arr):
+    for i in range(len(arr)):
+        if (isValidLevel(arr[:i] + arr[i+1:])):
+            return True
+    return False
 
 
 f = open("input.txt", "r")
@@ -25,8 +19,7 @@ sum = 0
 for line in f:
     level = line.strip().split(" ")
     level = [int(x) for x in level]
-    print(level, IsValidLevelWithError(level))
-    if (IsValidLevelWithError(level)):
+    if (isValidLevel(level) or IsValidLevelWithErrorForce(level)):
         sum += 1
 
 print(sum)
