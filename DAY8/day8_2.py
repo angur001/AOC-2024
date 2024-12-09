81675cfa9bf10fafa92f1b70cc00ad91099a0a31
angur001 <70037460+angur001@users.noreply.github.com>
2024-12-10 00:01:47 +0100

    DAY8

---
+++ DAY8/day8_2.py
@@ -0,0 +1,41 @@
+
+
+f = open("input.txt", "r")
+space =[[elem for elem in line.strip()] for line in f.readlines()]
+
+dictionary = {}
+for i in range(0, len(space[0])):
+    for j in range(0, len(space)):
+        if space[j][i] == ".":
+            continue
+        if space[j][i] in dictionary:
+            dictionary[space[j][i]].append((j, i))
+        else:
+            dictionary[space[j][i]] = [(j, i)]
+
+anti_nodes = set()
+
+def add_antinodes(arr):
+    global anti_nodes
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            d_lines = arr[i][0] - arr[j][0]
+            d_colones = arr[i][1] - arr[j][1]
+            pos1 = (arr[i][0], arr[i][1])
+            pos2 = (arr[j][0], arr[j][1])
+            while not(pos1[0] < 0 or pos1[0] >= len(space) or pos1[1] < 0 or pos1[1] >= len(space[0])):
+                anti_nodes.add(pos1)
+                print(f"added {pos1}")
+                pos1 = (pos1[0] + d_lines, pos1[1] + d_colones)
+
+            while not(pos2[0] < 0 or pos2[0] >= len(space) or pos2[1] < 0 or pos2[1] >= len(space[0])):
+                anti_nodes.add(pos2)
+                print(f"added {pos2}")
+                pos2 = (pos2[0] - d_lines, pos2[1] - d_colones)
+
+
+for frequencies in dictionary.values():
+    print(frequencies)
+    add_antinodes(frequencies)
+
+print(len(anti_nodes))
